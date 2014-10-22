7a5dd7a3b5b9b1af3fc7519027ca6a38092e8904
Dennis D. Silva <dsilva609@gmail.com>
2014-10-22 14:32:21 -0400

    fixed crc ansi declaration

--- Server/CRCEncoder.cpp
+++ Server/CRCEncoder.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-#define CRCANSI 1000000000000101
+#define CRCANSI "11000000000000101"
 
 class CRCEncoder
 {
@@ -11,18 +11,18 @@ public://change to private
 
 	void Calculate(string messege)
 	{
-		//messege = "1101010000000000000000";
-		messege = "1001";
+		messege = "1101010000000000000000";
+		//messege = "1001";
 		bitset<8> msg(messege);
 		bitset<16> poly(CRCANSI);
 
 		int result;
 
-		//	result = msg.to_ulong() / poly.to_ulong();
+		result = msg.to_ulong() % poly.to_ulong();
 
 		cout << msg.to_ulong() << endl;
 		cout << poly.to_ulong() << endl;
-		//cout << result << endl;
+		cout << result << endl;
 
 
 	}
