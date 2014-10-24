533ab7f5b4d5c4b1b9a4f30fda478e9e415b3522
Dennis D. Silva <dsilva609@gmail.com>
2014-10-24 01:37:59 -0400

    started encoding algorithm for first case

--- Server/HammingEncoder.cpp
+++ Server/HammingEncoder.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,6 +8,8 @@ class HammingEncoder
 public:
 	string Encode(string message)
 	{
+		CalculateHamming(message);
+
 		return "";
 	}
 
@@ -18,6 +21,33 @@ public:
 private:
 	string CalculateHamming(string message)
 	{
+		int numPBits;
+		int numOnes = 0;
+		string encodedMessage;
+
+		numPBits = floor(log2(message.length())) + 1;
+		cout << numPBits << endl;
+
+		for (int i = 1; i <= message.length(); i += 2)
+		{
+			if (message[i] == '1')
+				numOnes++;
+		}
+
+		if (isOdd(numOnes))
+			encodedMessage = "0" + message;
+		else
+			encodedMessage = "1" + message;
+
+		cout << "message: " << encodedMessage << endl;
+
 		return "";
+	}
+
+	bool isOdd(int num)
+	{
+		if (num % 2 == 1)
+			return true;
+		return false;
 	}
 };
