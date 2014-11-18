39f608926c6293b8ae7a590481151e8c0ef36063
dsilva609 <dsilva609@gmail.com>
2014-11-18 11:23:54 -0500

    removed crc, hamming, and error generator

--- Server/Main.cpp
+++ Server/Main.cpp
@@ -1,6 +1,5 @@
 #include "Server.cpp"
 #include "BinaryParser.cpp"
-#include "ErrorGenerator.cpp"
 
 using namespace std;
 
@@ -9,10 +8,8 @@ int main()
 	Server server;
 	BinaryParser parser;
 	vector<string> data;
-	ErrorGenerator generator;
 
 	data = parser.Parse("data.txt", false);
-	data = generator.Execute(data, 5);
 
 	server.Start("127.0.0.1", "44300", data);
 
