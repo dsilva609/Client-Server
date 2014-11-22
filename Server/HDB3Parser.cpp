f438f2865b16e32fe0f9a0d930e9f66d1acc7371
Dennis D. Silva <dsilva609@gmail.com>
2014-11-22 01:33:25 -0500

    added violation and balance pulse decider

--- Server/HDB3Parser.cpp
+++ Server/HDB3Parser.cpp
@@ -31,6 +31,7 @@ private:
 	{
 		int numZeroes = 0;
 		int numPulseCharacters = 0;
+		bool prevPulseWasPositive = false;
 		/*for each  (auto item in this->_resultData)
 		{
 		cout << item << endl;
@@ -43,26 +44,79 @@ private:
 
 		for (int i = 0; i < test.length(); i++)
 		{
-			if (test[i] == '+' || test[i] == '-')
+			if (test[i] == '+')
+			{
+				prevPulseWasPositive = true;
 				numPulseCharacters++;
+			}
+			else if (test[i] == '-')
+			{
+				prevPulseWasPositive = false;
+				numPulseCharacters++;
+			}
 
 			cout << "i: " << i << endl;
 			if (test[i] == '0')
 			{
 				if (test.length() - i >= 4)
 				{
-					if (test.substr(i, 4) == "0000")
+					if (test.substr(i, 4) == "0000") //change this substring to a violation string variable
 					{
 						cout << test.substr(i, 4) << endl;
 
+						if (isOdd(numPulseCharacters))
+						{
+							if (prevPulseWasPositive)
+							{
+								test.replace(i, 4, "-00-");
+								prevPulseWasPositive = false;
+							}
+							else
+							{
+								test.replace(i, 4, "+00+");
+								prevPulseWasPositive = true;
+							}
+							//test.replace(i, 4, "B00V");
+
+							//test.substr(i, 4) = "B00V";
+							numPulseCharacters = 1;
+						}
+						else
+						{
+							if (prevPulseWasPositive)
+							{
+								test.replace(i, 4, "000+");//does prevPulseWasPositive need to be updated?
+							}
+							else
+							{
+								test.replace(i, 4, "000-");
+							}
+							//test.substr(i, 4) = "000V";
+							numPulseCharacters = 1;
+						}
 						cout << "num pulse: " << numPulseCharacters << endl;
 
-						numPulseCharacters = 0;
+						//numPulseCharacters = 0;
 						i += 3;
 					}
 				}
 			}
+			/*else if (test[i] == '+' && prevPulseWasPositive && i != 0)
+			{
+			test[i] = '-';
+			prevPulseWasPositive = false;
+			}
+			else if (test[i] == '-' && !prevPulseWasPositive && i != 0)
+			{
+			test[i] = '+';
+			prevPulseWasPositive = true;
+			}
+			*/
+
+
 		}
+
+		cout << "test is now: " << test << endl;
 
 		/*
 			ALGORITHM
@@ -78,6 +132,24 @@ private:
 			---make sure to add the + or - from this substitution to pulse counter
 
 			---run through string again to make sure that there are no duplicate pulses ++ or --
+
+			Algorithm
+
+			Get the signal
+			Convert it to Alternate Mark Inversion (AMI) form
+			Look for 4 consecutive zeros and replace them with either 000V or B00V, Rule: If number of + and – at the left hand site of the consecutive 4 zeros is odd then use B00V else 000V
+			If it is 000V then V polarity should be as same as the polarity of the preceding pulse of those 4 consecutive zeros. If it is B00V, B and V are in same polarity but opposite to the preceding pulse of those 4 consecutive zeros.
+			Examples:
+
+			101000000000010
+
+			AMI : +0-0000000000+0
+
+			+0-000V000000+0
+			+0-000-B00V00+0
+			+0-000-+00+00-0
+
+
 			*/
 	}
 
@@ -103,6 +175,14 @@ private:
 
 		return binaryStr;
 	}
+
+	bool isOdd(int num)
+	{
+		if (num % 2 == 1)
+			return true;
+		return false;
+	}
+
 	void DecodeFromHDB3()
 	{
 		cout << "Does nothing yet" << endl;
